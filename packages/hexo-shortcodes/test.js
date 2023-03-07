eea6caf3d813fdcf561e165ad5eafd80786bbdd3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-08 05:05:20 +0700

    update project

--- packages/hexo-shortcodes/test.js
+++ packages/hexo-shortcodes/test.js
@@ -1,17 +1,19 @@
-// test render from parent site
+const { spawn } = require('git-command-helper');
 const path = require('path');
 const fs = require('fs-extra');
-const { writefile } = require('sbg-utility');
-const { spawn } = require('git-command-helper');
 
-const rootProject = path.join(__dirname, '../..');
-const siteProject = path.join(rootProject, 'site');
+const cwd = __dirname + '/test';
+process.cwd = () => cwd;
 
-const content = fs.readFileSync(path.join(__dirname, 'hexo-shortcodes.md')).toString();
-
-writefile(path.join(siteProject, 'source/docs/hexo-shortcodes/index.md'), content);
 (async function () {
-  await spawn('yarn', ['build'], { cwd: __dirname, shell: true });
-  await spawn('hexo', ['clean'], { cwd: siteProject, shell: true });
-  await spawn('hexo', ['server'], { cwd: siteProject, stdio: 'inherit', shell: true });
+  const spawnOpt = { cwd, stdio: 'inherit', shell: true };
+  const lockfile = path.join(cwd, 'yarn.lock');
+  if (!fs.existsSync(lockfile)) {
+    fs.writeFileSync(lockfile, '');
+    await spawn('yarn', ['install'], spawnOpt);
+  } else {
+    await spawn('yarn', ['add', '../'], spawnOpt);
+  }
+  await spawn('hexo', ['clean'], spawnOpt);
+  await spawn('hexo', ['server'], spawnOpt);
 })();
