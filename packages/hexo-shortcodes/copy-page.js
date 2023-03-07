5296ef7b9fba0f7add5ef3252f1485ee8db3bcf4
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-08 05:05:12 +0700

    feat: add page deployer

---
+++ packages/hexo-shortcodes/copy-page.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const fs = require('fs-extra');
+const { default: gch, spawn } = require('git-command-helper');
+
+// publish pages
+
+const src = path.resolve(__dirname, 'test/public');
+const dest = path.resolve(__dirname, '../../../dimaslanjaka.github.io/docs/hexo-shortcodes');
+(async function () {
+  const projectGit = new gch(__dirname, 'master');
+  const commit = await projectGit.latestCommit(__dirname);
+  const repo = new URL((await projectGit.getremote()).fetch.url).pathname.replace(/^\//, '');
+  const message = `chore: update from ${repo}@${commit}`;
+
+  if (fs.existsSync(dest)) await fs.emptyDir(dest);
+  await fs.copy(src, dest);
+
+  await spawn('git', ['add', '.'], { cwd: dest });
+  await spawn('git', ['commit', '-m', message], { cwd: dest });
+  await spawn('git', ['pull', '-X', 'ours'], { cwd: dest });
+  await spawn('git', ['push'], { cwd: dest });
+})();
