4e2bf165e98b1ef62b92a502597e0d7666003881
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-08 09:48:10 +0700

    update page builder

--- packages/hexo-shortcodes/page-build.js
+++ packages/hexo-shortcodes/page-build.js
@@ -4,20 +4,26 @@ const { default: gch, spawn } = require('git-command-helper');
 
 // publish pages
 
-const src = path.resolve(__dirname, 'test/public');
+const testDir = path.resolve(__dirname, 'test');
+const src = path.join(testDir, 'public');
 const dest = path.resolve(__dirname, '../../../dimaslanjaka.github.io/docs/hexo-shortcodes');
 (async function () {
+  // build start
+  await spawn('hexo', ['generate'], { cwd: testDir, shell: true });
+
+  // commit start
   const projectGit = new gch(__dirname, 'master');
   const commit = await projectGit.latestCommit(__dirname);
   const repo = new URL((await projectGit.getremote()).fetch.url).pathname.replace(/^\//, '');
   const message = `chore: update from ${repo}@${commit}`;
 
+  // copy start
   if (fs.existsSync(dest)) await fs.emptyDir(dest);
   await fs.copy(src, dest);
 
-  await spawn('git', ['add', '.'], { cwd: dest });
-  await spawn('git', ['commit', '-m', message], { cwd: dest });
-  await spawn('git', ['checkout', 'master'], { cwd: dest });
-  await spawn('git', ['pull', 'origin', 'master', '-X', 'ours'], { cwd: dest });
-  await spawn('git', ['push'], { cwd: dest });
+  await spawn('git', ['add', '.'], { cwd: dest, shell: true });
+  await spawn('git', ['commit', '-m', message], { cwd: dest, shell: true });
+  await spawn('git', ['checkout', 'master'], { cwd: dest, shell: true });
+  await spawn('git', ['pull', 'origin', 'master', '-X', 'ours'], { cwd: dest, shell: true });
+  await spawn('git', ['push'], { cwd: dest, shell: true });
 })();
