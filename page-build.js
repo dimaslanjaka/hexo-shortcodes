63347c10951c762c36866aa541900221a5b7c19e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-08 18:56:02 +0700

    refactor: update page builder

--- page-build.js
+++ page-build.js
@@ -6,7 +6,7 @@ const { default: gch, spawn } = require('git-command-helper');
 
 const testDir = path.resolve(__dirname, 'test');
 const src = path.join(testDir, 'public');
-const dest = path.resolve(__dirname, '../../../dimaslanjaka.github.io/docs/hexo-shortcodes');
+const dest = path.resolve(__dirname, '../dimaslanjaka.github.io/docs/hexo-shortcodes');
 (async function () {
   // build start
   await spawn('hexo', ['generate'], { cwd: testDir, shell: true });
@@ -18,14 +18,16 @@ const dest = path.resolve(__dirname, '../../../dimaslanjaka.github.io/docs/hexo-
   const message = `chore: update from ${repo}@${commit}`;
 
   // copy start
-  if (fs.existsSync(dest)) await fs.emptyDir(dest);
+  if (fs.existsSync(dest)) {
+    await fs.rm(dest, { recursive: true, force: true });
+    await fs.mkdir(dest);
+  }
   await fs.copy(src, dest, { overwrite: true });
 
   try {
-    console.log('add', dest);
+    await spawn('git', ['remote', 'set-url', 'origin', 'https://github.com/dimaslanjaka/docs.git'], { cwd: dest, shell: true })
     await spawn('git', ['add', '.'], { cwd: dest, shell: true });
-    console.log('commiting');
-    await spawn('git', ['commit', '-m', message], { cwd: dest, shell: true });
+    await spawn('git', ['commit', '-m', `"${message}"`], { cwd: dest, shell: true });
     await spawn('git', ['checkout', 'master'], { cwd: dest, shell: true });
     await spawn('git', ['pull', 'origin', 'master', '-X', 'ours'], { cwd: dest, shell: true });
     await spawn('git', ['push'], { cwd: dest, shell: true });
