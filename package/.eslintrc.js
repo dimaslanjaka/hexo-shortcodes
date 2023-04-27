348c8bcd331f3571d9061a860bf2d9cbc2a7693c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-27 17:38:14 +0700

    separate package

--- package/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: '../.eslintrc.js',
-  rules: {
-    '@typescript-eslint/adjacent-overload-signatures': 'off'
-  }
-};
--- package/dist/githubCard.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function githubCard(hexo: import('hexo')): void;
