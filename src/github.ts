453a96bc6ad5bafe25231acbee55734040055a2a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-29 00:00:00 +0700

    feat: add logname

--- src/github.ts
+++ src/github.ts
@@ -1,8 +1,12 @@
+import ansiColors from 'ansi-colors';
+
+const logname = ansiColors.magentaBright('hexo-shortcodes') + ansiColors.blueBright('(github)');
+
 export function githubEmbed(hexo: import('hexo')) {
   hexo.extend.tag.register(
     'github',
     async function (params) {
-      hexo.log.info(params);
+      hexo.log.info(logname, params);
 
       return '';
     },
