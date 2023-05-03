ba6c0de1f7c26c3cbf35e37d43fed4d89b005e61
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-04 06:39:49 +0700

    refactor: init husky and lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const config = {
+  '*.ts': ['npx prettier --write', 'eslint --fix'],
+  '*.html': ['npx eslint --fix', 'prettier --write'],
+  '*.scss': 'npx prettier --write',
+  '*.json': 'npx prettier --write'
+};
+
+module.exports = config;
