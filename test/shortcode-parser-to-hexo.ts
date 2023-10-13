536d109648d4bbfc7a633babac1be9c98a078b45
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-14 03:34:28 +0700

    chore: fixed fail parse spesific lines

--- test/shortcode-parser-to-hexo.ts
+++ test/shortcode-parser-to-hexo.ts
@@ -1,14 +1,16 @@
+import ansiColors from 'ansi-colors';
 import Hexo from 'hexo';
 import { gistEmbedTagRegister } from '../src/gist';
 import { githubEmbedTagRegister } from '../src/github';
 import { shortcodeParser, shortcodeParserResultToArrayAttrParam } from '../src/utils/shortcodeParser';
 
-const hexo = new Hexo(__dirname, { silent: true, debug: false });
+const hexo = new Hexo(__dirname, { silent: false, debug: false });
 // const parserGist = gist(hexo);
 
 const contents = [
   '{% github https://github.com/dimaslanjaka/static-blog-generator/blob/e8ef351552d57c5e28e016e39e78fef139a8e7b2/.github/workflows/build-beta.yml#L152-L158 %}',
-  '{% gist https://gist.github.com/dimaslanjaka/e7fc4c8778cdef48e1799de74a0c8949 %}'
+  '{% gist https://gist.github.com/dimaslanjaka/e7fc4c8778cdef48e1799de74a0c8949 %}',
+  '{% gist https://gist.github.com/dimaslanjaka/a6aa24a8fa7a13999ee3dac077fa21fe filename:anonymize-ip.php line:1-7 %}'
 ];
 
 async function main() {
@@ -23,7 +25,7 @@ async function main() {
   for (let i = 0; i < contents.length; i++) {
     const str = contents[i];
 
-    console.log('\n' + str + '\n');
+    console.log('\n' + ansiColors.green(str) + '\n');
 
     const parse = shortcodeParser(str);
     const arr = shortcodeParserResultToArrayAttrParam(parse);
